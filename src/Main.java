3e31e846fc7cf2e66217f925840eb51279921aa5
Кудринских Елизавета <lizokrus@gmail.com>
2024-10-02 20:41:18 +0300

    lab3_task2

---
+++ src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("\uD83E\uDEE0"); // 🫠 (тане обличчя)
+        System.out.println("\uD83E\uDEE5"); // 🫥 (невидиме обличчя)
+        System.out.println("\uD83E\uDD79"); // 🥹 (очі на мокрому місці)
+    }
+}
