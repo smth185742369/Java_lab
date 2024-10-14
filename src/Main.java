433635b8c8cc331e3fabaee82b21c9ac5d818197
Кудринских Елизавета <lizokrus@gmail.com>
2024-10-14 22:54:49 +0300

    Lab5_task3

--- src/Main.java
+++ src/Main.java
@@ -1,44 +1,29 @@
 import java.util.Scanner;
 
 public class Main {
-
-    // Метод для перевірки, чи є число простим
-    public static boolean isPrime(int num) {
-        if (num <= 1) return false;  // Числа менші за 2 не є простими
-        for (int i = 2; i <= Math.sqrt(num); i++) {
-            if (num % i == 0) {
-                return false;  // Якщо знайшли дільник, число не є простим
-            }
-        }
-        return true;
-    }
-
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        String longestString = "";
 
-        // Зчитуємо три числа
-        System.out.print("Введіть перше число: ");
-        int num1 = scanner.nextInt();
+        while (true) {
+            System.out.println("Введіть рядок (або 'done' для завершення):");
+            String input = scanner.nextLine();
 
-        System.out.print("Введіть друге число: ");
-        int num2 = scanner.nextInt();
+            if (input.equalsIgnoreCase("done")) {
+                break;
+            }
 
-        System.out.print("Введіть третє число: ");
-        int num3 = scanner.nextInt();
-
-        System.out.println("Числа, що не є простими:");
-
-        // Перевіряємо кожне з чисел і виводимо, якщо воно не просте
-        if (!isPrime(num1)) {
-            System.out.println(num1);
+            if (input.length() > longestString.length()) {
+                longestString = input;
+            }
         }
 
-        if (!isPrime(num2)) {
-            System.out.println(num2);
+        if (!longestString.isEmpty()) {
+            System.out.println("Найдовший рядок: " + longestString);
+        } else {
+            System.out.println("Жоден рядок не був введений.");
         }
 
-        if (!isPrime(num3)) {
-            System.out.println(num3);
-        }
+        scanner.close();
     }
 }
