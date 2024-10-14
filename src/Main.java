01e506d6cd525dfd5b930926875190fba0fd1e1a
Кудринских Елизавета <lizokrus@gmail.com>
2024-10-14 22:06:02 +0300

    Lab3_Task2

--- src/Main.java
+++ src/Main.java
@@ -2,38 +2,30 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        // Створення об'єкта Scanner для читання введення з клавіатури
         Scanner scanner = new Scanner(System.in);
 
-        // Зчитування трьох цілих чисел
-        System.out.print("Введіть число X: ");
-        int X = scanner.nextInt();
-        System.out.print("Введіть число Y: ");
-        int Y = scanner.nextInt();
-        System.out.print("Введіть число Z: ");
-        int Z = scanner.nextInt();
+        // Запитуємо користувача ввести рядок
+        System.out.print("Введіть рядок: ");
+        String input = scanner.nextLine();
 
-        // Перевірка умови X > Y > Z > 0
-        if (X > Y && Y > Z && X > 0) {
-            double Q1 = Math.pow(X, 3) - Y - Math.pow(Z, 2);
-            System.out.println("Q = X^3 - Y - Z^2: " + Q1);
-        }
-
-        // Перевірка умови Y > X >= Z
-        if (Y > X && X >= Z) {
-            double Q2 = Math.pow(X, 2) + Y * Z;
-            System.out.println("Q = X^2 + Y * Z: " + Q2);
-        }
-
-        // Перевірка умови Z >= Y > X
-        if (Z >= Y && Y > X) {
-            double Q3 = X * Y + Math.pow(Z, X);
-            System.out.println("Q = X * Y + Z^X: " + Q3);
-        }
-
-        // Загальна формула, якщо всі умови не виконуються
-        double Q4 = -X - Y - Z;
-        System.out.println("Q = -X - Y - Z: " + Q4);
-
+        // Закриваємо сканер
         scanner.close();
+
+        // Перевіряємо, чи є в рядку бодай одна велика літера
+        boolean hasUpperCase = false;
+        for (char c : input.toCharArray()) {
+            if (Character.isUpperCase(c)) {
+                hasUpperCase = true;
+                break;
+            }
+        }
+
+        // Виводимо результат
+        if (hasUpperCase) {
+            System.out.println("ТАК");
+        } else {
+            System.out.println("НІ");
+        }
     }
 }
